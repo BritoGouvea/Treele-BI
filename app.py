69e29623558b62b040427a3a5da9fed27508056a
BritoGouvea <39095057+BritoGouvea@users.noreply.github.com>
2023-08-26 11:41:28 -0300

    classe_obra

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output
-from treele_dados import Obra, Insumo, SolicitaçãoDeCompras, Item_SolicitaçãoDeCompras
+from dados import obras
+from sienge_classes.OrçamentoDeObra import Orçamento
 
 # app = dash.Dash(__name__)
 
@@ -13,3 +14,12 @@ from treele_dados import Obra, Insumo, SolicitaçãoDeCompras, Item_Solicitaçã
 
 # if __name__ == '__main__':
 #     app.run_server(debug=True)
+
+orçamento_tjm = Orçamento.carregar("./treele_dados/bases/orçamento_obra_8.json")
+# orçamento_tjm = Orçamento({
+#     'obra': 8,
+#     'insumos': [],
+#     'planilhas': []
+# })
+
+# orçamento_tjm.salvar()
